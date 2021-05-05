91a1d72eb4c6ac96ff9b6fde2ad496f608d80c01
Usarneme <jamestcoop@gmail.com>
2021-05-05 15:49:04 -0700

    :hammer: Pass - creates functionality to pass returning day of week from a given date
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- WeekdayFinder/Models/WeekdayFinder.cs
+++ WeekdayFinder/Models/WeekdayFinder.cs
@@ -1,3 +1,4 @@
+using System;
 
 namespace WeekdayFinderNamespace
 {
@@ -5,7 +6,13 @@ namespace WeekdayFinderNamespace
   {
     public static string GetDayOfWeekFromDate(string date)
     {
-      return "Fail";
+      string[] dateStrings = date.Split("/");
+      int[] dateInts = Array.ConvertAll(dateStrings, s => int.Parse(s));
+      int year = dateInts[2];
+      int month = dateInts[0];
+      int day = dateInts[1];
+      DateTime dateObject = new DateTime(year, month, day);
+      return dateObject.DayOfWeek.ToString();
     }
   }
 }
